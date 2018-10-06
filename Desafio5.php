1f4eaf509fa388aa200208a2a22f2aa182472bc2
Bernardino Campos <bcampos@Pobox.com>
2018-10-06 15:56:00 -0300

    Mais melhorias de Performance

--- Desafio5.php
+++ Desafio5.php
@@ -3,18 +3,27 @@
 	ini_set('error_reporting', 0);
 	ini_set('memory_limit', -1);
 	ini_set('zend.enable_gc', 0);
+	#ini_set('xdebug.profiler_output_dir','/Storage/Desafio5/');
+	#ini_set('xdebug.profiler_enable',1);
+
+	function echoStr($str) {
+		echo $str;
+	}
+
+	if (strlen($argv[1])<10)		die ("\nFaltou o Arquivo\n\n");
 
 	$var = json_decode(file_get_contents($argv[1], "r"),true);
 
-	$cont['global'] = 0;
-	$acum['global'] = 0;
+	$cont['gl'] = 0;
+	$acum['gl'] = 0;
 	$numFunc = [];
 	$sobrenome = [];
 
 	foreach ($var['areas'] as $value)
 		$areas[$value['codigo']] = $value['nome'];
 
-	$menor['global'][0]['salario']=99999999;
+	$megl[0]['salario']=99999999;
+	$magl = [];
 
 	foreach($var['funcionarios'] as $func) {
 
@@ -22,69 +31,71 @@
 		$area = $func['area'];
 		$sname = $func['sobrenome'];
 
-		if ($salario > $maior['global'][0]['salario'])
-			$maior['global']=[$func];
- 		else if ($salario == $maior['global'][0]['salario'])
-			$maior['global'][]=$func;
+		$mag = (float)$magl[0]['salario'];
+		if ($salario > $mag)
+			$magl=[$func];
+ 		else if ($salario == $mag)
+			$magl[]=$func;
 
-		if ($salario < $menor['global'][0]['salario'])
-			$menor['global']=[$func];
-		else if ($salario == $menor['global'][0]['salario'])
-			$menor['global'][]=$func;
+		$meg = (float)$megl[0]['salario'];
+		if ($salario < $meg)
+			$megl=[$func];
+		else if ($salario == $meg)
+			$megl[]=$func;
 
-		if ($salario > $maior[$area][0]['salario'])
+		$ma = (float)$maior[$area][0]['salario'];
+		if ($salario > $ma)
 			$maior[$area]=[$func];
- 		else if ($salario == $maior[$area][0]['salario'])
+ 		else if ($salario == $ma)
 			$maior[$area][]=$func;
 
-		if (!IsSet($menor[$area]) || $salario < $menor[$area][0]['salario'])
+		$mea = (float)$menor[$area][0]['salario'];
+		if (!IsSet($menor[$area]) || $salario < $mea)
 			$menor[$area]=[$func];
-		else if ($salario == $menor[$area][0]['salario'])
+		else if ($salario == $mea)
 			$menor[$area][]=$func;
 
-		if ($salario > $sobrenome[$sname][0]['salario'] )
+		$sn = (float)$sobrenome[$sname][0]['salario'];
+		if ($salario >$sn )
 			$sobrenome[$sname] = [$func];
-		else if ($salario == $sobrenome[$sname][0]['salario'])
+		else if ($salario == $sn)
 			$sobrenome[$sname][] = $func;
 
-		@$acum['global']+=$salario;
-		@$cont['global']++;
+		@$agl+=$salario;
+		@++$cgl;
 		@$acum[$area]+=$salario;
-		@$cont[$area]++;
-		@$numFunc[$area]++;
-		@$sNome[$sname]++;
+		@++$cont[$area];
+		@++$numFunc[$area];
+		@++$sNome[$sname];
 	}
 
 	asort($numFunc);
 
+	foreach($magl as $func)
+		echoStr("global_max|{$func['nome']} {$func['sobrenome']}|".number_format($magl[0]['salario'],2,".","")."\n");
+	foreach($megl as $func)
+		echoStr("global_min|{$func['nome']} {$func['sobrenome']}|".number_format($megl[0]['salario'],2,".","")."\n");
+	echoStr("global_avg|".number_format($agl/$cgl,2,".","")."\n");
+
 	foreach($maior as $key=>$value) {
-		if ($key=='global') {
-			foreach($maior[$key] as $func)
-				echo "global_max|{$func['nome']} {$func['sobrenome']}|".number_format($maior[$key][0]['salario'],2,".","")."\n";
-			foreach($menor[$key] as $func)
-				echo "global_min|{$func['nome']} {$func['sobrenome']}|".number_format($menor[$key][0]['salario'],2,".","")."\n";
-			echo "global_avg|".number_format($acum[$key]/$cont[$key],2,".","")."\n";
-		}
-		else {
-			foreach($maior[$key] as $func)
-				echo "area_max|{$areas[$key]}|{$func['nome']} {$func['sobrenome']}|".number_format($maior[$key][0]['salario'],2,".","")."\n";
-			foreach($menor[$key] as $func)
-				echo "area_min|{$areas[$key]}|{$func['nome']} {$func['sobrenome']}|".number_format($menor[$key][0]['salario'],2,".","")."\n";
-			echo "area_avg|{$areas[$key]}|".number_format($acum[$key]/$cont[$key],2,".","")."\n";
-		}
+		foreach($maior[$key] as $func)
+			echoStr("area_max|{$areas[$key]}|{$func['nome']} {$func['sobrenome']}|".number_format($maior[$key][0]['salario'],2,".","")."\n");
+		foreach($menor[$key] as $func)
+			echoStr("area_min|{$areas[$key]}|{$func['nome']} {$func['sobrenome']}|".number_format($menor[$key][0]['salario'],2,".","")."\n");
+		echoStr("area_avg|{$areas[$key]}|".number_format($acum[$key]/$cont[$key],2,".","")."\n");
 	}
 
 	reset($numFunc);
 	$total = current($numFunc);
 	while ($total==current($numFunc)) {
-		echo "least_employees|".$areas[key($numFunc)]."|".current($numFunc)."\n";
+		echoStr("least_employees|".$areas[key($numFunc)]."|".current($numFunc)."\n");
 		next($numFunc);
 	}
 
 	end($numFunc);
 	$total = current($numFunc);
 	while ($total==current($numFunc)) {
-		echo "most_employees|".$areas[key($numFunc)]."|".current($numFunc)."\n";
+		echoStr("most_employees|".$areas[key($numFunc)]."|".current($numFunc)."\n");
 		prev($numFunc);
 	}
 
@@ -92,5 +103,5 @@
 		if ($sNome[$key]==1)	continue;
 
 		foreach($sobrenome[$key] as $func)
-			echo "last_name_max|$key|{$func['nome']} {$func['sobrenome']}|".number_format($value[0]['salario'],2,".","")."\n";
+			echoStr("last_name_max|$key|{$func['nome']} {$func['sobrenome']}|".number_format($value[0]['salario'],2,".","")."\n");
 	}
